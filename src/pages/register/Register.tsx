91f3aaac0767646c1a50874a1d50feb76bfc1eb5
David Chen <chen7david@me.com>
2024-05-28 12:49:05 -0400

    chore: setup router and created new example pages

---
+++ src/pages/register/Register.tsx
@@ -0,0 +1,9 @@
+export const Register = () => {
+  return (
+    <div className="flex h-screen bg-blue-200 justify-center items-center">
+      <div className="flex flex-col gap-3 card text-blue-600 align-center">
+        <h1 className="text-blue-400 text-3xl font-bold">Register Page!</h1>
+      </div>
+    </div>
+  )
+}
