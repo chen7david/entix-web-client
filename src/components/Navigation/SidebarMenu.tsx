43bcc9b1868326c6c335fb546d94dac2f586f3fc
David M. Chen <chen7david@me.com>
2024-06-03 16:17:59 -0400

    chore: setup login logout flow

---
+++ src/components/Navigation/SidebarMenu.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import type { MenuProps } from 'antd'
+import { useNavigate } from 'react-router-dom'
+import { Menu } from 'antd'
+import { sidebarMenuData } from './SidebarMenuData'
+
+export interface SidebarMenuProps {
+  onClick: () => void
+}
+
+export const SidebarMenu = ({ onClick }: SidebarMenuProps) => {
+  const [current, setCurrent] = useState('mail')
+  const navigate = useNavigate()
+
+  const menuOnClick: MenuProps['onClick'] = (e) => {
+    setCurrent(e.key)
+    navigate(e.key)
+    onClick()
+  }
+
+  return (
+    <Menu
+      onClick={menuOnClick}
+      selectedKeys={[current]}
+      mode="vertical"
+      items={sidebarMenuData}
+    />
+  )
+}
