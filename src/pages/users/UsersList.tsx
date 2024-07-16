054da660e6f9735da5cdfbe272673bf96b9723ef
David M. Chen <chen7david@me.com>
2024-07-16 10:18:31 -0400

    feat: added create user form

--- src/pages/users/UsersList.tsx
+++ src/pages/users/UsersList.tsx
@@ -1,7 +1,31 @@
 import { useEffect, useState } from 'react'
 import { http } from './../../http'
 import { Button, Table, TableColumnsType } from 'antd'
-import { IViewUserDto, IPaginatedFilterResponse } from 'entix-shared'
+import {
+  IViewUserDto,
+  IPaginatedFilterResponse,
+  ICreateUserDto,
+} from 'entix-shared'
+import { UserCreateModal } from './UserCreateModal'
+
+function getAge(dobString: string) {
+  // Parse the date string to a Date object
+  const dob = new Date(dobString)
+
+  // Get today's date
+  const today = new Date()
+
+  // Calculate the age
+  let age = today.getFullYear() - dob.getFullYear()
+  const monthDiff = today.getMonth() - dob.getMonth()
+
+  // Check if the birthday has occurred yet this year
+  if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dob.getDate())) {
+    age--
+  }
+
+  return age
+}
 
 const columns: TableColumnsType<IViewUserDto> = [
   {
@@ -11,19 +35,20 @@ const columns: TableColumnsType<IViewUserDto> = [
     responsive: ['md'],
   },
   {
-    title: 'id',
-    dataIndex: 'id',
-    key: 'id',
+    title: 'username',
+    dataIndex: 'username',
+    key: 'username',
+  },
+  {
+    title: 'age',
+    dataIndex: 'date_of_birth',
+    key: 'date_of_birth',
+    render: (text) => getAge(text),
   },
   {
     title: 'email',
     dataIndex: 'email',
     key: 'email',
-  },
-  {
-    title: 'username',
-    dataIndex: 'username',
-    key: 'username',
   },
 ]
 
@@ -31,8 +56,9 @@ export const UsersList = () => {
   const [users, setUsers] = useState<IViewUserDto[]>([])
 
   async function getUsers() {
-    const { data } =
-      await http.get<IPaginatedFilterResponse<IViewUserDto[]>>('/api/v1/users')
+    const { data } = await http.get<IPaginatedFilterResponse<IViewUserDto[]>>(
+      '/api/v1/users?sortBy=id:desc',
+    )
     setUsers(data.data)
   }
 
@@ -40,15 +66,14 @@ export const UsersList = () => {
     getUsers()
   }, [])
 
-  async function createUser() {
-    await http.post('/api/v1/users', {})
+  async function createUser(formData: ICreateUserDto) {
+    const { data: user } = await http.post('/api/v1/users', formData)
+    setUsers((prevUsers) => [...prevUsers, user])
   }
 
   return (
     <div className="">
-      <Button onClick={createUser} className="mb-5">
-        New User
-      </Button>
+      <UserCreateModal onSubmit={createUser} />
       <Table rowKey="id" dataSource={users} columns={columns} />
     </div>
   )
