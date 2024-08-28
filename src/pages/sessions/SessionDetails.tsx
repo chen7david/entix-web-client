cf19156d95983ab74bf9ef0c46c7db6f62176f9b
David Chen <chen7david@me.com>
2024-08-27 21:19:29 -0400

    chore: added central point updates

--- src/pages/sessions/SessionDetails.tsx
+++ src/pages/sessions/SessionDetails.tsx
@@ -3,14 +3,9 @@ import { PageContainer } from '@/components/Layout/PageContainer'
 import { Toolbar } from '@/components/Layout/Toolbar'
 import { useQuery } from '@tanstack/react-query'
 import { useParams } from 'react-router-dom'
-import { SessionPointsCard } from './SessionPointsCard'
+import { SessionPointsCard, SessionPointsCardRef } from './SessionPointsCard'
 import { useRef } from 'react'
 import { Button } from 'antd'
-
-// Define the type of the ref
-type SessionPointsCardRef = {
-  save: () => void
-}
 
 export const SessionDetails = () => {
   const { id } = useParams()
@@ -18,7 +13,19 @@ export const SessionDetails = () => {
   const cardRefs = useRef<(SessionPointsCardRef | null)[]>([])
 
   const handleSaveAll = () => {
-    cardRefs.current.forEach((ref) => ref?.save())
+    cardRefs.current.forEach((ref) => ref?.savePoints())
+  }
+
+  const plusOne = () => {
+    cardRefs.current.forEach((ref) => ref?.plusOne())
+  }
+
+  const plusFive = () => {
+    cardRefs.current.forEach((ref) => ref?.plusFive())
+  }
+
+  const plusTen = () => {
+    cardRefs.current.forEach((ref) => ref?.plusTen())
   }
 
   const sessionUsersQuery = useQuery({
@@ -29,7 +36,18 @@ export const SessionDetails = () => {
   return (
     <>
       <Toolbar className="bg-white shadow-sm flex gap-2">
-        <Button onClick={handleSaveAll}>Save Points</Button>
+        <Button type="text" onClick={handleSaveAll}>
+          Save
+        </Button>
+        <Button type="text" onClick={plusOne}>
+          +1
+        </Button>
+        <Button type="text" onClick={plusFive}>
+          +5
+        </Button>
+        <Button type="text" onClick={plusTen}>
+          +10
+        </Button>
       </Toolbar>
       <PageContainer className="flex flex-col gap-2">
         <div className="flex gap-4">
