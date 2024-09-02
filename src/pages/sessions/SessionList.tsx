0342f33efb2276260743a4d74eef96db63350b10
David M. Chen <chen7david@me.com>
2024-09-02 16:57:56 -0400

    chore: fixed basic date range issues for wallet and sessions

--- src/pages/sessions/SessionList.tsx
+++ src/pages/sessions/SessionList.tsx
@@ -15,16 +15,23 @@ dayjs.extend(utc)
 export const SessionList = () => {
   const [form] = Form.useForm()
   const { ref, inView } = useInView()
+
+  // Set today's start and end dates
+  const todayStart = dayjs().utc().startOf('day').toISOString()
+  const todayEnd = dayjs().utc().endOf('day').toISOString()
+
   const [searchParams, setSearchParams] = useSearchParams({
-    startDate: dayjs().utc().subtract(1, 'month').toISOString(),
-    endDate: dayjs().utc().toISOString(),
+    startDate: todayStart,
+    endDate: todayEnd,
     groupId: '',
     name: '',
     limit: '10',
   })
+
   const limit = searchParams.get('limit') || ''
-  const startDate = searchParams.get('startDate') || ''
-  const endDate = searchParams.get('endDate') || ''
+  const startDate = searchParams.get('startDate') || todayStart
+  const endDate = searchParams.get('endDate') || todayEnd
+
   const usePaginatedQuery = useInfiniteQuery({
     queryKey: ['sessions', { startDate, endDate }],
     initialPageParam: null,
@@ -49,14 +56,70 @@ export const SessionList = () => {
   const onSearch = async () => {
     // console.log(v)
   }
+
   return (
     <>
       <Toolbar className="bg-white shadow-sm">
         <Form form={form} layout="inline" onFinish={onSearch}>
           <Form.Item>
             <DatePicker.RangePicker
+              presets={[
+                {
+                  label: 'Today',
+                  value: [dayjs().startOf('day'), dayjs().endOf('day')],
+                },
+                {
+                  label: 'Tomorrow',
+                  value: [
+                    dayjs().add(1, 'day').startOf('day'),
+                    dayjs().add(1, 'day').endOf('day'),
+                  ],
+                },
+                {
+                  label: 'Next Month',
+                  value: [
+                    dayjs().add(1, 'month').startOf('month'),
+                    dayjs().add(1, 'month').endOf('month'),
+                  ],
+                },
+                {
+                  label: 'Next Year',
+                  value: [
+                    dayjs().add(1, 'year').startOf('year'),
+                    dayjs().add(1, 'year').endOf('year'),
+                  ],
+                },
+                {
+                  label: 'Last 7 Days',
+                  value: [
+                    dayjs().add(-7, 'd').startOf('day'),
+                    dayjs().endOf('day'),
+                  ],
+                },
+                {
+                  label: 'Last 14 Days',
+                  value: [
+                    dayjs().add(-14, 'd').startOf('day'),
+                    dayjs().endOf('day'),
+                  ],
+                },
+                {
+                  label: 'Last 30 Days',
+                  value: [
+                    dayjs().add(-30, 'd').startOf('day'),
+                    dayjs().endOf('day'),
+                  ],
+                },
+                {
+                  label: 'Last 90 Days',
+                  value: [
+                    dayjs().add(-90, 'd').startOf('day'),
+                    dayjs().endOf('day'),
+                  ],
+                },
+              ]}
               size="large"
-              defaultValue={[dayjs(startDate), dayjs(endDate)]}
+              defaultValue={[dayjs(todayStart), dayjs(todayEnd)]}
               onChange={(dates) => {
                 if (!dates) return
                 const [startDate, endDate] = dates
